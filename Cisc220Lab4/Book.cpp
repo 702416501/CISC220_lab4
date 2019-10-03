ed0b0eae927c329924437b2d1b22c538e28844c2
aDeAbreu <47873062+702416501@users.noreply.github.com>
2019-10-03 19:07:18 -0400

    Update Book.cpp

--- Cisc220Lab4/Book.cpp
+++ Cisc220Lab4/Book.cpp
@@ -8,36 +8,61 @@
 #include "Book.hpp"
 #include <iostream>
 #include <stdlib.h>
+#include <array>
 using namespace std;
 
 
 Book::Book(string firstn, string lastn, string bookn) {
-	ratings[10] = 0;
 	nFirst = firstn;
 	nLast = lastn;
 	nBook = bookn;
 	yrPub = NULL;
 
-}
+	ratings =  new int[10];
+		for (int i = 0; i < 10; i++){
+			ratings[i] = 0;
+		}
+	}
 
-Book::Book(string firstn, string lastn, string bookn, int pubyr, int ratings){
-	//ratings[] = 0;
+Book::Book(string firstn, string lastn, string bookn, int pubyr, int *rts){
 	nFirst = firstn;
 	nLast = lastn;
 	nBook = bookn;
 	yrPub = pubyr;
+
+	*ratings = *rts;
 }
+
+
+
+
 
 Book::~Book(){//destructor
+	cout<<nBook<<" has been deleted"<<endl;
 }
 
-double Book::avgRating(int rts[]){
-	double x = 0;
 
-	return x;
+
+double Book::avgRating(int *rts){
+	int sum = 0;
+	double tot = 0;
+	double avg;
+	for (int i = 0; i< 10; i++){
+		sum += ratings[i];
+		tot++;
+	}
+
+	avg = sum/tot ;
+	return avg;
+
 }
 
-void Book::printBkInfo(){
+
+
+
+
+
+void Book::printBkInfo() {
 	cout<<"Book name: "<<nBook<<endl;
 	cout<<"Author: "<<nFirst<<" "<<nLast<<endl;
 	cout<<"Year published: "<<yrPub<<endl;
